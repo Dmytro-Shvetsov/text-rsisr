Improve trainer's logging logic. Fix wrong normalization in the dataloader. Other improvements

src/datamodule.py
@@ -4,7 +4,6 @@ from re import S
 
 import albumentations as albu
 import cv2
-import pytorch_lightning as pl
 import torch
 
 from torch.utils.data import DataLoader
@@ -46,13 +45,13 @@ class DataModule:
 
         self._lr_transforms = albu.Compose([
             albu.Resize(*config.lr_img_size, cv2.INTER_CUBIC),
-            albu.ToFloat(),
-            albu.Normalize(config.norm_means, config.norm_stds, max_pixel_value=255.0), # outputs values in [0.; 1.] range
+            albu.ToFloat(max_value=255.0), # outputs values in [0.; 1.] range
+            albu.Normalize(config.norm_means, config.norm_stds, max_pixel_value=1.0), 
         ])
         self._hr_transforms = albu.Compose([
             albu.Resize(*config.hr_img_size, cv2.INTER_CUBIC),
-            albu.ToFloat(),
-            albu.Normalize(config.norm_means, config.norm_stds), # outputs values in [0.; 1.] range
+            albu.ToFloat(max_value=255.0), # outputs values in [0.; 1.] range
+            albu.Normalize(config.norm_means, config.norm_stds, max_pixel_value=1.0),
         ])
 
     def setup(self, stage=None):
@@ -82,7 +81,7 @@ class DataModule:
 
     def test_dataloader(self):
         return DataLoader(self._test_dst, 
-                          batch_size=2, 
+                          batch_size=1, 
                           shuffle=False, 
-                          num_workers=self._cfg.num_workers,
+                          num_workers=1,
                           pin_memory=True)

src/models/cyclegan.py
@@ -1,9 +1,8 @@
 from tkinter import Variable
 from typing import Callable
 import cv2
-from importlib_metadata import itertools
+import itertools
 import numpy as np
-import pytorch_lightning as pl
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -97,7 +96,7 @@ class CycleGAN(SuperResolutionModel):
         return self.G_AB(x)
 
     def parse_outputs(self, outputs):
-        return denormalize(outputs, self.means, self.stds).mul_(255).add_(0.5).byte()
+        return denormalize(outputs, self.means, self.stds).mul_(255).add_(0.5).clamp_(0, 255).byte()
 
     def generators_step(self, batch):
         real_A, real_B, _ = batch
@@ -138,7 +137,7 @@ class CycleGAN(SuperResolutionModel):
         })
         return loss_G, (fake_A, fake_B), logs
 
-    def discriminator_A_step(self, batch, fake_imgs, fake_buffer=True):
+    def discriminator_A_step(self, batch, fake_imgs):
         real_A, real_B, _ = batch
 
         fake_A, _ = fake_imgs
@@ -161,7 +160,7 @@ class CycleGAN(SuperResolutionModel):
         })
         return loss_D_A, logs
 
-    def discriminator_B_step(self, batch, fake_imgs, fake_buffer=True):
+    def discriminator_B_step(self, batch, fake_imgs):
         real_A, real_B, _ = batch
 
         _, fake_B = fake_imgs
@@ -218,15 +217,12 @@ class CycleGAN(SuperResolutionModel):
     @torch.no_grad()
     def eval_step(self, batch):
         batch[0] = self.preprocess(batch[0])
-        _, y, _ = batch
+        x, y, _ = batch
 
         loss_G, fake_imgs, G_logs = self.generators_step(batch)
         loss_D_A, D_A_logs = self.discriminator_A_step(batch, fake_imgs)
         loss_D_B, D_B_logs = self.discriminator_B_step(batch, fake_imgs)
         
-        impsnr = psnr(fake_imgs, y)
-        imssim = ssim(fake_imgs, y)
-
         logs = OrderedDict({'losses': OrderedDict(), 'images': OrderedDict()})
         logs['losses'].update(G_logs)
         logs['losses']['loss_D'] = (loss_D_A + loss_D_B) / 2
@@ -240,7 +236,9 @@ class CycleGAN(SuperResolutionModel):
         ))
 
         logs['metrics'] = OrderedDict((
-            ('PSNR', impsnr),
-            ('SSIM', imssim)
+            ('G_AB_PSNR', psnr(fake_imgs[1], y)),
+            ('G_AB_SSIM', ssim(fake_imgs[1], y)),
+            ('G_BA_PSNR', psnr(fake_imgs[0], x)),
+            ('G_BA_SSIM', ssim(fake_imgs[0], x)),
         ))
         return logs

src/models/esrgan.py
@@ -4,7 +4,6 @@ import cv2
 from sklearn.feature_extraction import image
 import torch
 import numpy as np
-import pytorch_lightning as pl
 from torch import nn
 from torch.nn import functional as F
 from torchmetrics.functional import ssim, psnr
@@ -78,7 +77,7 @@ class ESRGAN(SuperResolutionModel):
         return self.generator(x)
 
     def parse_outputs(self, outputs):
-        return denormalize(outputs, self.means, self.stds).mul_(255).add_(0.5).byte()
+        return denormalize(outputs, self.means, self.stds).mul_(255).add_(0.5).clamp_(0, 255).byte()
 
     def train(self, mode: bool = True):
         ret = super().train(mode)

tools/train.py
@@ -2,6 +2,8 @@ import os
 import json
 import argparse
 
+import torch
+
 from src.datamodule import DataModule
 from src.utils.config_reader import Config, object_from_dict
 from src.trainers.trainer import Trainer
@@ -18,17 +20,29 @@ def seed_everything(seed):
     torch.cuda.manual_seed(seed)
 
 
+def dump_test_results(results, model_dir):
+    for k, v in results.items():
+        if isinstance(v, torch.Tensor):
+            v = v.cpu().item()
+        results[k] = round(v, 5)
+    fp = os.path.join(model_dir, 'test_results.json')
+    with open(fp, 'w') as fid:
+        json.dump(results, fid, indent=4)
+    print(f'Saved test results at: {repr(fp)}')
+
+
 def train(args):
     config = Config(args.config)
     seed_everything(config.seed)
     datamodule = DataModule(config)
     datamodule.setup()
     trainer = Trainer(config)
+    trainer.restore_checkpoint()
     trainer.fit(datamodule.train_dataloader(), datamodule.val_dataloader())
     datamodule.setup('test')
     results = trainer.test(datamodule.test_dataloader())
-    with open(os.path.join(trainer.model_dir, 'test_results.json'), 'w') as fid:
-        json.dump(results, fid)
+    results['epoch'] = trainer.current_epoch
+    dump_test_results(results, trainer.model_dir)
 
 
 if __name__ == '__main__':